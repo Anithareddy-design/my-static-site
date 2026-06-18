6f8519b50ac86fbab74d67c07859af8386cbc85b
Anithareddy-design <anithareddyyerredla9@gmail.com>
2026-06-18 11:27:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const revealElements = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  revealElements.forEach((element) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+    const revealPoint = 120;
+
+    if (elementTop < windowHeight - revealPoint) {
+      element.classList.add("active");
+    }
+  });
+});
